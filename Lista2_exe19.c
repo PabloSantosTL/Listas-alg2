9a4f0a95d1c5e39df9b421d7b296c5ff19791e8c
PabloSantosTL <pablo.santos4@estudante.ifms.edu.br>
2025-07-15 19:54:22 -0400

    Create Lista2_exe19.c

---
+++ Lista2_exe19.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+struct Prod{int cod; char desc[100]; double preco;};
+int main(){
+    struct Prod p;
+    FILE *f=fopen("PRODUTOS.txt","r+");
+    while(fscanf(f,"%d %99s %lf",&p.cod,p.desc,&p.preco)==3){
+        if(p.preco>100) p.preco+=10;
+        fseek(f,-sizeof(p),SEEK_CUR);
+        fprintf(f,"%d %s %.2f\n",p.cod,p.desc,p.preco);
+    }
+    fclose(f);
+    return 0;
+}
