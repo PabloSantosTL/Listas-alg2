4bc64d472eb0f8327e371dd7749c2af2b940e2eb
PabloSantosTL <pablo.santos4@estudante.ifms.edu.br>
2025-07-15 19:54:47 -0400

    Create Lista2_exe20.c

---
+++ Lista2_exe20.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+struct Prod{int cod; char desc[100]; double preco;};
+int main(){
+    struct Prod p; char c;
+    scanf(" %c",&c);
+    FILE *f=fopen("PRODUTOS.txt","r");
+    while(fscanf(f,"%d %99s %lf",&p.cod,p.desc,&p.preco)==3)
+        if(p.desc[0]==c) printf("%d %s %.2f\n",p.cod,p.desc,p.preco);
+    fclose(f);
+    return 0;
+}
