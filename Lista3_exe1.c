a7d52de928ae84c4142e1a865fc1c48adab4be4b
PabloSantosTL <pablo.santos4@estudante.ifms.edu.br>
2025-07-15 21:17:37 -0400

    Create Lista3_exe1.c

---
+++ Lista3_exe1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    printf("sizeof(char) = %zu byte(s)\n", sizeof(char));
+    printf("sizeof(int) = %zu byte(s)\n", sizeof(int));
+    printf("sizeof(float) = %zu byte(s)\n", sizeof(float));
+    printf("sizeof(double) = %zu byte(s)\n", sizeof(double));
+    return 0;
+}
