9fd12d17313c1598489c06ac12bf2c07851cd371
PabloSantosTL <pablo.santos4@estudante.ifms.edu.br>
2025-07-15 21:18:15 -0400

    Create Lista3_exe3.c

---
+++ Lista3_exe3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct Cadastro { char nome[100]; int idade; char endereco[200]; };
+struct Cadastro* criarCadastro(int N){
+    struct Cadastro *vet = malloc(N * sizeof(*vet));
+    for(int i=0;i<N;i++){
+        printf("Cadastro %d:\n", i+1);
+        printf("Nome: "); scanf(" %99[^"]", vet[i].nome);
+        printf("Idade: "); scanf("%d", &vet[i].idade);
+        printf("Endereco: "); scanf(" %199[^"]", vet[i].endereco);
+    }
+    return vet;
+}
